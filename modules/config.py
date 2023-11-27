93bea956dd5a5de99132b6d9061615f35a8fd93e
nightnekko <148694833+nightnekko@users.noreply.github.com>
2023-11-27 21:49:48 +0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -262,12 +262,12 @@ lora_downloads = get_config_item_or_set_default(
     key='lora_downloads',
     default_value={
         "sd_xl_offset_example-lora_1.0.safetensors": "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors",
-        "eye_lora": "https://civitai.com/api/download/models/6433" #!
+        "eye_lora.safetensors": "https://civitai.com/api/download/models/6433" #!
     },
     validator=lambda x: isinstance(x, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in x.items())
 )
 print ('!', lora_downloads) #!
-lora_downloads['eye_lora'] = 'https://civitai.com/api/download/models/6433' #!
+lora_downloads['eye_lora.safetensors'] = 'https://civitai.com/api/download/models/6433' #!
 print ('!', lora_downloads) #!
 embeddings_downloads = get_config_item_or_set_default(
     key='embeddings_downloads',
